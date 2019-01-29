b73b00165f8753df7a758665d7a487269af20b7a
Aral Balkan <aral@ind.ie>
2019-01-29 18:00:52 +0000

    Refactor to decouple model/view & passphrase/key/database generation

--- client/index.js
+++ client/index.js
@@ -52,6 +52,32 @@ function to_hex(input) {
   return str;
 }
 
+//
+// Model
+//
+
+const model = {
+  keys: {
+    nodeReadKeyInHex: null,
+    nodeWriteKeyInHex: null,
+    nodeReadKey: null,
+    nodeWriteKey: null,
+    nodeDiscoveryKey: null,
+    nodeDiscoveryKeyInHex: null
+  }
+}
+
+//
+// viewModel
+//
+
+const kSignIn = 'Sign in'
+const kSignUp = 'Sign up'
+const viewModel = {
+  action: kSignUp
+}
+
+
 // HTML elements.
 const setupForm = document.getElementById('setupForm')
 const buttonAndProgressIndicator = document.getElementById('buttonAndProgressIndicator')
@@ -112,22 +138,11 @@ function resetForm() {
   privateEncryptionKeyTextField.value = ''
 }
 
+
 function logError(error) {
   errorsTextArea.value += error
 }
 
-function generatePassphrase () {
-  resetForm()
-
-  showProgressIndicator()
-
-  // On next tick, so the interface has a chance to update.
-  setTimeout(() => {
-    const passphrase = generateEFFDicewarePassphrase.entropy(100)
-    setupForm.elements.passphrase.value = passphrase.join(' ')
-    generateKeys()
-  }, 0)
-}
 
 function showDetails() {
   detailSections = document.getElementsByClassName('details')
@@ -136,6 +151,7 @@ function showDetails() {
   }
 }
 
+
 function hideDetails() {
   detailSections = document.getElementsByClassName('details')
   for (var i = 0; detailSections[i]; i++) {
@@ -143,19 +159,23 @@ function hideDetails() {
   }
 }
 
+
 function showProgressIndicator() {
   changeButton.style.display = 'none';
   indeterminateProgressIndicator.style.display = 'block';
 }
+
 
 function hideProgressIndicator() {
   changeButton.style.display = 'block';
   indeterminateProgressIndicator.style.display = 'none';
 }
 
+
 function hideButton () {
   buttonAndProgressIndicator.style.display = 'none'
 }
+
 
 function clearOutputFields() {
   publicSigningKeyTextField.value = ''
@@ -164,212 +184,262 @@ function clearOutputFields() {
   privateEncryptionKeyTextField.value = ''
 }
 
-function generateKeys() {
-  const passphrase = setupForm.elements.passphrase.value
-  const domain = setupForm.elements.domain.value
+async function initialiseNode(passphrase = null) {
 
-  session25519(domain, passphrase, (error, keys) => {
+  showProgressIndicator()
 
-    hideProgressIndicator()
+  if (passphrase === null) {
+    console.log('Initialising new node with new domain')
+
+    const passphrase = await generatePassphrase()
+    setupForm.elements.passphrase.value = passphrase
+
+    const domain = setupForm.elements.domain.value
+    model.keys = await generateKeys(passphrase, domain)
+
+    // Display the keys.
+    publicSigningKeyTextField.value = model.keys.nodeReadKeyInHex
+    privateSigningKeyTextArea.value = model.keys.nodeWriteKeyInHex
+    publicEncryptionKeyTextField.value = model.keys.publicEncryptionKeyInHex
+    privateEncryptionKeyTextField.value = model.keys.privateEncryptionKeyInHex
+
+    createDatabase()
+
+    // Update the view
     hideButton()
     showDetails()
-
-    if (error) {
-      logError(error.message)
-      return
-    }
-
+  } else {
     //
-    // Convert the keys first to ArrayBuffer and then to
-    // Node’s implementation of Buffer, which is what
-    // hypercore expected.
+    // A passphrase has been passed. Replicate an existing domain’s database.
     //
-    // If you try to pass an ArrayBuffer instead, you get
-    // the following error:
-    //
-    // Error: key must be at least 16, was given undefined
-    //
+    console.log('Initialising new node with existing domain')
+    alert(`Todo: sign in with passphrase ${passphraseTextField.value}`)
+    // 1. Generate keys using the passphrase
+    // 2. Generate hyperdb with local key based on main key (and verifiable by a different node)
+    // 3. Use hyperswarm to find peers and use out of band messages to request authentication
+  }
 
-    const nodeReadKeyInHex = to_hex(keys.publicSignKey)
-    const nodeWriteKeyInHex = to_hex(keys.secretSignKey)
-    const nodeReadKey = Buffer.from(keys.publicSignKey.buffer)
-    const nodeWriteKey = Buffer.from(keys.secretSignKey.buffer)
+  hideProgressIndicator()
+}
 
-    // Calculate the discovery key from the read key (the hyphalink).
-    const nodeDiscoveryKey = discoveryKey(nodeReadKey)
-    const nodeDiscoveryKeyInHex = nodeDiscoveryKey.toString('hex')
 
-    let db = null
-    let stream = null
-    let updateInterval = null
+// Returns a promise that resolves to a passphrase.
+function generatePassphrase () {
+  return new Promise (resolve => {
+    // On next tick, so the interface has a chance to update.
+    setTimeout(() => {
+      const passphrase = generateEFFDicewarePassphrase.entropy(100).join (' ')
+      resolve(passphrase)
+    }, 0)
+  })
+}
 
-    console.log(`Creating new hyperdb with read key ${nodeReadKeyInHex} and write key ${nodeWriteKeyInHex}`)
 
-    // Create a new hypercore using the newly-generated key material.
-    db = hyperdb((filename) => ram(filename), nodeReadKey, {
-      createIfMissing: false,
-      overwrite: false,
-      valueEncoding: 'json',
-      secretKey: nodeWriteKey,
-      storeSecretKey: false //,
-      // onwrite: (index, data, peer, next) => {
-      //   console.log(`db: [onWrite] index = ${index}, peer = ${peer}, data:`)
-      //   console.log(data)
-      //   // TypeError: next is not a function
-      //   // next()
-      // }
+// Returns a promise that generates Ed25519 signing keys and
+// Curve25519 encryption keys by deriving them from the passed
+// passphrase and using the domain as the salt.
+function generateKeys(passphrase, domain) {
+  return new Promise((resolve, reject) => {
+
+    session25519(domain, passphrase, (error, keys) => {
+
+      if (error) {
+        logError(error.message)
+        reject(error)
+      }
+
+      //
+      // Convert the keys first to ArrayBuffer and then to
+      // Node’s implementation of Buffer, which is what
+      // hypercore expected.
+      //
+      // If you try to pass an ArrayBuffer instead, you get
+      // the following error:
+      //
+      // Error: key must be at least 16, was given undefined
+      //
+      const nodeReadKey = Buffer.from(keys.publicSignKey.buffer)
+      const nodeDiscoveryKey = discoveryKey(nodeReadKey)
+      const nodeDiscoveryKeyInHex = nodeDiscoveryKey.toString('hex')
+
+      const nodeKeys = {
+        nodeReadKey,
+        nodeDiscoveryKey,
+        nodeDiscoveryKeyInHex,
+        nodeReadKeyInHex: to_hex(keys.publicSignKey),
+        nodeWriteKeyInHex: to_hex(keys.secretSignKey),
+        nodeWriteKey: Buffer.from(keys.secretSignKey.buffer),
+        publicEncryptionKeyInHex: to_hex(keys.publicKey),
+        privateEncryptionKeyInHex: to_hex(keys.secretKey)
+      }
+
+      resolve(nodeKeys)
+    })
+  })
+}
+
+
+function createDatabase() {
+  let db = null
+  let stream = null
+  let updateInterval = null
+
+  console.log(`Creating new hyperdb with read key ${model.nodeReadKeyInHex} and write key ${model.nodeWriteKeyInHex}`)
+
+  // Create a new hypercore using the newly-generated key material.
+  db = hyperdb((filename) => ram(filename), model.nodeReadKey, {
+    createIfMissing: false,
+    overwrite: false,
+    valueEncoding: 'json',
+    secretKey: model.nodeWriteKey,
+    storeSecretKey: false
+    // Note: do not define onWrite(). Leads to errors.
+  })
+
+
+  db.on('ready', () => {
+    const dbKey = db.key
+    const dbKeyInHex = to_hex(dbKey)
+
+    console.log(`db: [Ready] ${dbKeyInHex}`)
+
+    blinkSignal('ready')
+    generatedTextField.value = 'Yes'
+
+    // Display the local key for the local writer.
+    console.log(db.local)
+    localKeyTextField.value = db.local.key.toString('hex')
+
+    const watcher = db.watch('/table', () => {
+      console.log('Database updated!')
+      db.get('/table', (error, values) => {
+        console.log(values)
+
+        blinkSignal('change')
+        console.log('db [change: get]', values)
+
+        // New data is available on the db. Display it on the page.
+        const obj = values[0].value
+        for (let [key, value] of Object.entries(obj)) {
+          dbContentsTextArea.value += `${key}: ${value}\n`
+        }
+      })
     })
 
 
-    db.on('ready', () => {
-      const dbKey = db.key
-      const dbKeyInHex = to_hex(dbKey)
+    // Hypercore db is ready: connect to web socket and start replicating.
+    const remoteStream = webSocketStream(`wss://localhost/hypha/${dbKeyInHex}`)
 
-      console.log(`db: [Ready] ${dbKeyInHex}`)
+    const localStream = db.replicate({
+      // If we remove the encrypt: false, we get an error on the server:
+      // Pipe closed for c4a99bc919c23d9c12b1fe440a41488141263e59fb98288388b578e105ad2523 Remote message is larger than 8MB (max allowed)
+      // Why is this and what’s the encryption that we’re turning off here and what effects does this have on privacy and security? (TODO: file issue)
+      encrypt: false,
+      live: true
+    })
 
-      blinkSignal('ready')
-      generatedTextField.value = 'Yes'
+    // Create a duplex stream.
+    //
+    // What’s actually happening:
+    //
+    // remoteStream.write -> localStream.read
+    // localStream.write -> remoteStream.read
+    pump(
+      remoteStream,
+      localStream,
+      remoteStream,
+      (error) => {
+        console.log(`Pipe closed for ${dbKeyInHex}`, error && error.message)
+        logError(error.message)
+      }
+    )
 
-      // Display the local key for the local writer.
-      console.log(db.local)
-      localKeyTextField.value = db.local.key.toString('hex')
+    // Also join a WebRTC swarm so that we can peer-to-peer replicate
+    // this hypercore (browser to browser).
+    const webSwarm = swarm(signalhub(model.keys.nodeDiscoveryKeyInHex, ['https://localhost:444']))
+    webSwarm.on('peer', function (remoteWebStream) {
 
-      const watcher = db.watch('/table', () => {
-        console.log('Database updated!')
-        db.get('/table', (error, values) => {
-          console.log(values)
+      console.log(`WebSwarm [peer for ${model.keys.nodeReadKeyInHex} (discovery key: ${model.keys.nodeDiscoveryKeyInHex})] About to replicate.`)
 
-          blinkSignal('change')
-          console.log('db [change: get]', values)
-
-          // New data is available on the db. Display it on the page.
-          const obj = values[0].value
-          for (let [key, value] of Object.entries(obj)) {
-            dbContentsTextArea.value += `${key}: ${value}\n`
-          }
-        })
-      })
-
-
-      // Hypercore db is ready: connect to web socket and start replicating.
-      const remoteStream = webSocketStream(`wss://localhost/hypha/${dbKeyInHex}`)
-
-      const localStream = db.replicate({
-        // If we remove the encrypt: false, we get an error on the server:
-        // Pipe closed for c4a99bc919c23d9c12b1fe440a41488141263e59fb98288388b578e105ad2523 Remote message is larger than 8MB (max allowed)
-        // Why is this and what’s the encryption that we’re turning off here and what effects does this have on privacy and security? (TODO: file issue)
-        encrypt: false,
+      // Create the local replication stream.
+      const localReplicationStream = db.replicate({
         live: true
       })
 
-      // Create a duplex stream.
-      //
-      // What’s actually happening:
-      //
-      // remoteStream.write -> localStream.read
-      // localStream.write -> remoteStream.read
       pump(
-        remoteStream,
-        localStream,
-        remoteStream,
+        remoteWebStream,
+        localReplicationStream,
+        remoteWebStream,
         (error) => {
-          console.log(`Pipe closed for ${dbKeyInHex}`, error && error.message)
-          logError(error.message)
+          console.log(`[WebRTC] Pipe closed for ${model.keys.nodeReadKeyInHex}`, error && error.message)
         }
       )
+    })
 
-      // Also join a WebRTC swarm so that we can peer-to-peer replicate
-      // this hypercore (browser to browser).
-      const webSwarm = swarm(signalhub(nodeDiscoveryKeyInHex, ['https://localhost:444']))
-      webSwarm.on('peer', function (remoteWebStream) {
+    //
+    // TEST
+    //
+    const NUMBER_TO_APPEND = 3
+    let counter = 0
 
-        console.log(`WebSwarm [peer for ${nodeReadKeyInHex} (discovery key: ${nodeDiscoveryKeyInHex})] About to replicate.`)
+    const intervalToUpdateInMS = 500
+    updateInterval = setInterval(() => {
+      counter++
+      if (counter === NUMBER_TO_APPEND) {
+        console.log(`Reached max number of items to append (${NUMBER_TO_APPEND}). Will not add any more.`)
+        clearInterval(updateInterval)
+        updateInterval = null
+      }
 
-        // Create the local replication stream.
-        const localReplicationStream = db.replicate({
-          live: true
-        })
-
-        pump(
-          remoteWebStream,
-          localReplicationStream,
-          remoteWebStream,
-          (error) => {
-            console.log(`[WebRTC] Pipe closed for ${nodeReadKeyInHex}`, error && error.message)
-          }
-        )
-      })
-
-      //
-      // TEST
-      //
-      const NUMBER_TO_APPEND = 3
-      let counter = 0
-
-      const intervalToUpdateInMS = 500
-      updateInterval = setInterval(() => {
-        counter++
-        if (counter === NUMBER_TO_APPEND) {
-          console.log(`Reached max number of items to append (${NUMBER_TO_APPEND}). Will not add any more.`)
-          clearInterval(updateInterval)
-          updateInterval = null
+      const key = nextId()
+      const value = Math.random()*1000000000000000000 // simple random number
+      let obj = {}
+      obj[key] = value
+      db.put('/table', obj, (error, o) => {
+        console.log('Put callback')
+        if (error) {
+          logError(error)
+          return
         }
-
-        const key = nextId()
-        const value = Math.random()*1000000000000000000 // simple random number
-        let obj = {}
-        obj[key] = value
-        db.put('/table', obj, (error, o) => {
-          console.log('Put callback')
-          if (error) {
-            logError(error)
-            return
-          }
-          console.log('  Feed', o.feed)
-          console.log('  Sequence:', o.seq)
-          console.log('  Key:', o.key)
-          console.log('  Value:', o.value)
-        })
-      }, intervalToUpdateInMS)
-    })
-
-    db.on('error', (error) => {
-      console.log(`db [Error] ${error}`)
-      blinkSignal('error')
-      logError(error)
-    })
-
-    db.on('download', (index, data) => {
-      blinkSignal('download')
-      console.log(`db [Download] index = ${index}, data = ${data}`)
-    })
-
-    db.on('upload', (index, data) => {
-      blinkSignal('upload')
-      console.log(`db [Upload] index = ${index}, data = ${data}`)
-    })
-
-    db.on('append', () => {
-      blinkSignal('append')
-      console.log('db [Append]')
-    })
-
-    db.on('sync', () => {
-      blinkSignal('sync')
-      console.log('db [Sync]')
-    })
-
-    db.on('close', () => {
-      blinkSignal('close')
-      console.log('db [Close]')
-    })
-
-    // Display the keys.
-    publicSigningKeyTextField.value = to_hex(keys.publicSignKey)
-    privateSigningKeyTextArea.value = to_hex(keys.secretSignKey)
-    publicEncryptionKeyTextField.value = to_hex(keys.publicKey)
-    privateEncryptionKeyTextField.value = to_hex(keys.secretKey)
+        console.log('  Feed', o.feed)
+        console.log('  Sequence:', o.seq)
+        console.log('  Key:', o.key)
+        console.log('  Value:', o.value)
+      })
+    }, intervalToUpdateInMS)
   })
+
+  db.on('error', (error) => {
+    console.log(`db [Error] ${error}`)
+    blinkSignal('error')
+    logError(error)
+  })
+
+  db.on('download', (index, data) => {
+    blinkSignal('download')
+    console.log(`db [Download] index = ${index}, data = ${data}`)
+  })
+
+  db.on('upload', (index, data) => {
+    blinkSignal('upload')
+    console.log(`db [Upload] index = ${index}, data = ${data}`)
+  })
+
+  db.on('append', () => {
+    blinkSignal('append')
+    console.log('db [Append]')
+  })
+
+  db.on('sync', () => {
+    blinkSignal('sync')
+    console.log('db [Sync]')
+  })
+
+  db.on('close', () => {
+    blinkSignal('close')
+    console.log('db [Close]')
+  })
+
 }
 
 // Creates passphrase (and keys) when the form is submitted.
@@ -379,29 +449,21 @@ function onFormSubmit (event) {
 
   event.preventDefault()
 
-  if (model.action === kSignUp) {
-    generatePassphrase()
+  if (viewModel.action === kSignUp) {
+    initialiseNode()
   } else {
-    alert(`Todo: sign in with passphrase ${passphraseTextField.value}`)
 
-    // 1. Generate keys using the passphrase
-    // 2. Generate hyperdb with local key based on main key (and verifiable by a different node)
-    // 3. Use hyperswarm to find peers and use out of band messages to request authentication
+    initialiseNode(passphraseTextField.value)
+
   }
-}
-
-const kSignIn = 'Sign in'
-const kSignUp = 'Sign up'
-model = {
-  action: kSignUp
 }
 
 function updateInitialState() {
   const passphrase = passphraseTextField.value
-  model.action = (passphrase === '') ? kSignUp : kSignIn
-  changeButton.innerHTML = model.action
+  viewModel.action = (passphrase === '') ? kSignUp : kSignIn
+  changeButton.innerHTML = viewModel.action
 
-  if (model.action === kSignIn) {
+  if (viewModel.action === kSignIn) {
     // Validate that the passphrase exists solely of diceware words
     // and has at least eight words (as we know the password generation aims
     // for at least 100 bits of entropy. Seven words has ~90 bits.)
